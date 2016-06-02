f6e10c302d46afd38a3baac324016ba39e85d073
chris <chris@localhost>
2016-06-01 23:32:00 -0500

    doc de CostoUniforme.java lista

--- src/proyectoia/noinformada/CostoUniforme.java
+++ src/proyectoia/noinformada/CostoUniforme.java
@@ -126,6 +126,11 @@ public class CostoUniforme {
         return child;
     }
 
+    /**
+     * Checks if a node is goal or not and takes items and the suit in case it finds them
+     * @param node to be checked
+     * @return true if this is the goal, false otherwise
+     */
     public boolean expand(Node node) {
         //gets the value of the cell where the robot is
 
@@ -148,6 +153,9 @@ public class CostoUniforme {
         return goal;
     }
 
+    /**
+     * Search algorithm using Uniform Cost
+     */
     public void uniformCost() {
         long startTime = System.currentTimeMillis();
         boolean loop = true;
@@ -174,6 +182,7 @@ public class CostoUniforme {
                     }
                 } else {
                     List<Node> path = solution.getPathFromRoot();
+                    System.out.println("Path to Victory!");
                     for (int i = 0; i < path.size(); i++) {
                         Point pos = this.getEnvironment().findRobot(path.get(i).getState());
                         System.out.println((int) pos.getX() + ", " + (int) pos.getY());
@@ -191,29 +200,49 @@ public class CostoUniforme {
     }
 
     /**
-     *
-     * @return
+     * Returns the root node 
+     * @return The root node
      */
     public Node getRoot() {
         return root;
     }
 
+    /**
+     * Returns the world of the problem
+     * @return World of the problem
+     */
     public Entorno getEnvironment() {
         return environment;
     }
 
+    /**
+     * Returns the possible nodes to be expanded
+     * @return List of nodes to be expanded
+     */
     public List<Node> getFrontier() {
         return frontier;
     }
 
+    /**
+     * returns the expanded nodes
+     * @return List of expanded nodes
+     */
     public List<Node> getExplored() {
         return explored;
     }
 
+    /**
+     * Returns the solution of the problem
+     * @return The solution node of the problem
+     */
     public Node getSolution() {
         return solution;
     }
 
+    /**
+     * Returns the running time of the search
+     * @return time of the search
+     */
     public long getTotalTime() {
         return totalTime;
     }
